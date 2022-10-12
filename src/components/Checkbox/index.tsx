4edad212e4131804f4ac8996a496284eecd05833
gamadv <lealdev.py@gmail.com>
2022-10-12 18:22:37 -0300

    feat: Checkbox component created with Radix Checkbox

---
+++ src/components/Checkbox/index.tsx
@@ -0,0 +1,17 @@
+import * as CheckboxPrimitive from "@radix-ui/react-checkbox";
+import { Check } from "phosphor-react";
+import { clsx } from "clsx";
+
+export type TCheckbox = {};
+
+export function Checkbox({}: TCheckbox) {
+  return (
+    <CheckboxPrimitive.Root
+      className={clsx("w-6 h-6 p-[2px] bg-gray-800 rounded", {})}
+    >
+      <CheckboxPrimitive.Indicator asChild>
+        <Check weight="bold" className="h-5 w-5 text-cyan-500" />
+      </CheckboxPrimitive.Indicator>
+    </CheckboxPrimitive.Root>
+  );
+}
