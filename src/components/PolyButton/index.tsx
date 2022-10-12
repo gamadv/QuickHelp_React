2c4a55cc6bff123a14c9eee886be11eb8ef98864
gamadv <lealdev.py@gmail.com>
2022-10-12 18:22:48 -0300

    feat: PolyButton component created

--- src/components/PolyButton/index.tsx
+++ src/components/PolyButton/index.tsx
@@ -1,11 +1,17 @@
 import { ButtonHTMLAttributes } from "react";
 import { Slot } from "@radix-ui/react-slot";
+import { clsx } from "clsx";
 
-type TPolyButton = ButtonHTMLAttributes<HTMLButtonElement> & {
+export type TPolyButton = ButtonHTMLAttributes<HTMLButtonElement> & {
   asChild?: boolean;
 };
 
-export function PolyButton(props: TPolyButton) {
-  const Component = props.asChild ? Slot : "button";
-  return <Component className="px-4 py-2 rounded bg-yellow-700" {...props} />;
+export function PolyButton({asChild, ...props}: TPolyButton) {
+  const Component = asChild ? Slot : "button";
+  return (
+    <Component
+      className={clsx("px-4 py-2 rounded bg-yellow-700 text-white", {})}
+      {...props}
+    />
+  );
 }
