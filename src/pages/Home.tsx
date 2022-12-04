01bda41c341d1e6adc59c176530f95b80829dd7c
gamadv <lealdev.py@gmail.com>
2022-12-03 23:01:54 -0300

    feat: Input Styled created

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,20 +1,20 @@
 import { PolyButton } from "../components/PolyButton";
+import InputStyled from "../components/UI/InputStyled";
 
 export function Home() {
   return (
-    <main className="flex max-w-[1200px] m-auto py-6 bg-yellow-400">
+    <main className="flex flex-col max-w-[1200px] m-auto py-6 bg-yellow-400">
       <section className="flex items-center max-w-md justify-between m-auto gap-4">
         <PolyButton>Default</PolyButton>
         <PolyButton asChild>
-            <div>
-                as DIV
-            </div>
+          <div>as DIV</div>
         </PolyButton>
         <PolyButton asChild>
-            <a href="#">
-                as Anchor
-            </a>
+          <a href="#">as Anchor</a>
         </PolyButton>
+      </section>
+      <section>
+        <InputStyled />
       </section>
     </main>
   );
